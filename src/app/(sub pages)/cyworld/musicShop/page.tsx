93905399ad0bbb9766699d24ca88b2889483be30
박지원 <jackgg12322@gmail.com>
2025-06-04 23:34:48 +0900

    design : "MusicShop Layout setup"

--- src/app/(sub pages)/cyworld/musicShop/page.tsx
+++ src/app/(sub pages)/cyworld/musicShop/page.tsx
@@ -4,9 +4,15 @@ import React from 'react';
 
 export default function MusicShop() {
   return (
-    <div className="flex flex-col bg-bgColors-secondary">
+    <div className="flex flex-col w-full px-[325px]">
       <Header />
-      <BgmList />
+      <div className="flex flex-row gap-8 mt-8 ">
+        <aside className="w-full bg-white h-[650px]"></aside>
+
+        <BgmList />
+
+        <aside className="w-full bg-white h-[650px]"></aside>
+      </div>
     </div>
   );
 }
