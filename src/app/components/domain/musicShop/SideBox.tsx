dea70e5b3d032a31ba681645540497388a8cb4f0
박지원 <jackgg12322@gmail.com>
2025-07-03 20:25:55 +0900

    style:"SideBox ui 수정(SideBox.tsx)"

--- src/app/components/domain/musicShop/SideBox.tsx
+++ src/app/components/domain/musicShop/SideBox.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode } from 'react';
 
 export interface SideBoxProps {
   children?: ReactNode;
@@ -7,7 +7,7 @@ export interface SideBoxProps {
 
 export const SideBox = ({ children }: SideBoxProps) => {
   return (
-    <div className="w-full flex flex-col bg-white border-solid border-[1px] border-bgColors-quaternary rounded-[4px] m-b-[20px] p-[15px]">
+    <div className="w-full flex flex-col bg-white border-[0.2rem] m-b-[20px] p-[15px]">
       {children}
     </div>
   );
