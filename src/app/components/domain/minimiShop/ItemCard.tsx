4b39a7f61dca2b60c286e8a8ea42d7b86e7a4798
박지원 <jackgg12322@gmail.com>
2025-07-03 20:30:05 +0900

    refactor:"ItemCard New Icon & Dotori Icon 로직 수정"

--- src/app/components/domain/minimiShop/ItemCard.tsx
+++ src/app/components/domain/minimiShop/ItemCard.tsx
@@ -4,10 +4,17 @@ import Image, { StaticImageData } from 'next/image';
 interface ItemCardProps {
   minimiUrl: string;
   minimiName: string;
-  price: number;
+  price?: number;
   onClick?: () => void;
+  createdAt?: string;
 }
-const ItemCard = ({ minimiUrl, minimiName, price, onClick }: ItemCardProps) => {
+const ItemCard = ({
+  minimiUrl,
+  minimiName,
+  price,
+  onClick,
+  createdAt,
+}: ItemCardProps) => {
   return (
     <div
       className="flex flex-col gap-[0.3rem] cursor-pointer"
@@ -20,24 +27,28 @@ const ItemCard = ({ minimiUrl, minimiName, price, onClick }: ItemCardProps) => {
         height={100}
         className="border-solid border-[0.1rem] border-bgColors-quinary bg-white rounded-[0.8rem] relative"
       />
-      <Image
-        src={'https://storage.googleapis.com/cyworld-bucket-2/new.png'}
-        alt="새로운 아이템 N 아이콘"
-        width={13}
-        height={13}
-        className="absolute translate-x-[5rem] translate-y-[0.3rem]"
-      />
+      {createdAt && (
+        <Image
+          src={'https://storage.googleapis.com/cyworld-bucket-2/new.png'}
+          alt="새로운 아이템 N 아이콘"
+          width={13}
+          height={13}
+          className="absolute translate-x-[5rem] translate-y-[0.3rem]"
+        />
+      )}
       <p>{minimiName}</p>
 
-      <div className="flex flex-row  items-center text-center gap-[0.2rem] size-[15px]">
-        <Image
-          src={'https://storage.googleapis.com/cyworld-bucket-2/dotori.png'}
-          alt="도토리 이미지"
-          width={100}
-          height={100}
-        />
-        <p>{price}</p>
-      </div>
+      {price && (
+        <div className="flex flex-row  items-center text-center gap-[0.2rem] size-[15px]">
+          <Image
+            src={'https://storage.googleapis.com/cyworld-bucket-2/dotori.png'}
+            alt="도토리 이미지"
+            width={100}
+            height={100}
+          />
+          <p>{price}</p>
+        </div>
+      )}
     </div>
   );
 };
