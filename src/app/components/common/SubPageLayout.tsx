795fa257570c04ee961299aa00129842f37778ee
박지원 <jackgg12322@gmail.com>
2025-07-27 21:53:45 +0900

    fix:"모바일 기기 접근 제한 안내 문구 수정 & tailwind 모바일 사이즈 수정"

--- src/app/components/common/SubPageLayout.tsx
+++ src/app/components/common/SubPageLayout.tsx
@@ -11,10 +11,10 @@ export default function SubPageLayout({ children }: SubPagesLayoutProps) {
   const [isMobile, setIsMobile] = useState(false);
 
   const handleResize = () => {
-    window.innerWidth < 461 ? setIsMobile(true) : setIsMobile(false);
+    window.innerWidth <= 768 ? setIsMobile(true) : setIsMobile(false);
   };
   useEffect(() => {
-    window.innerWidth < 461 ? setIsMobile(true) : setIsMobile(false);
+    window.innerWidth <= 768 ? setIsMobile(true) : setIsMobile(false);
     window.addEventListener('resize', handleResize);
     // 컴포넌트 언마운트 시 이벤트 리스너 삭제
     return () => {
@@ -25,10 +25,19 @@ export default function SubPageLayout({ children }: SubPagesLayoutProps) {
   return (
     <>
       {isMobile ? (
-        <div className="text-center text-lg text-gray-700 py-20">
-          모바일 기기에서는 사용이 불가능합니다.
-          <Link href="https://cyworldbeta.duckdns.org/cyworld/">
-            메인으로 이동하기
+        <div className="flex flex-col gap-2 text-center text-lg text-gray-700 py-20 ">
+          <p>
+            죄송합니다. 현재 모바일 기기에서는 사용이 불가능합니다.
+            <br />
+            빠른 시일 내에 업데이트 되도록 노력하겠습니다.
+            <br />
+            감사합니다.
+          </p>
+          <Link
+            href="https://cyworldbeta.duckdns.org/cyworld/"
+            className="text-white border-solid border-2 border-textColors-primary rounded-md px-4 py-2 bg-bgColors-primary"
+          >
+            메인 페이지로 이동하기
           </Link>
         </div>
       ) : (
